2408498339bb627682276c6a1fb4fda026b80eec
Hector Rios <oj.rios@gmail.com>
2016-02-09 10:16:12 +0100

    Tapper Extreme final with some tweaks

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -10,16 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    // MARK: Controller Outlets
+    @IBOutlet weak var logoImg : UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn:UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+
+    // MARK: controller variables
+    var currentTapCount = 0
+    var maxTapCount = 0
+    
+    //MARK: IBActions
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != "" {
+            
+            logoImg.hidden = true
+            playBtn.hidden = true
+            howManyTapsTxt.hidden = true
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTapCount = Int(howManyTapsTxt.text!)!
+            currentTapCount = 0
+            
+            self.updateTapsLbl()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onCoinBtnPressed(sender: UIButton) {
+        
+        if isGameOver() {
+            restartTheGame()
+            return
+        }
+        
+        currentTapCount++
+        self.updateTapsLbl()
+        
+        
     }
-
-
+    
+    //MARK: controller functions
+    func updateTapsLbl() {
+        tapsLbl.text = "\(currentTapCount) Taps"
+    }
+    
+    func isGameOver() -> Bool {
+        return (currentTapCount >= maxTapCount)
+    }
+    
+    func restartTheGame() {
+        maxTapCount = 0
+        howManyTapsTxt.text = ""
+        self.tapsLbl.text = ""
+        
+        logoImg.hidden = false
+        playBtn.hidden = false
+        howManyTapsTxt.hidden = false
+        
+        tapBtn.hidden = true
+        tapsLbl.hidden = true
+    }
+    
 }
 
